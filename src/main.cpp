0cf3a6d4ed701d40c7891e15a98d1f7a4f2828e7
Sam Daitzman <sam@daitzman.com>
2022-10-18 12:03:17 -0400

    add comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,50 @@
 #include <Arduino.h>
 
+// Next time to send buttonpress signal
 unsigned long next_pressed = 0;
 
+// Should we run a debug loop that presses the button every 10 seconds?
+#define debug_loop 0
+
 void setup() {
+  // Begin Serial communications. Wait 800ms to stabilize.
   Serial.begin(115200);
   delay(800);
+
+  // Set ESP32 pin 25 to output, and turn it off.
   pinMode(GPIO_NUM_25, OUTPUT);
   digitalWrite(GPIO_NUM_25, LOW);
 }
 
 void loop() {
+  // Get and print the current time for logging purposes
   unsigned long time = millis();
   Serial.print(time);
   Serial.print("\t");
+
+  // We want to check if the button is currently being pressed by the user.
+  // Switching to INPUT mode will cause the Braun Series 8 dock to read a press.
+  // So, we make it such a brief press (5ms) that the unit simply ignores it.
+  // Then switch back to the regular OUTPUT mode.
   pinMode(GPIO_NUM_25, INPUT_PULLUP);
   delay(5);
   Serial.print(digitalRead(GPIO_NUM_25));
   pinMode(GPIO_NUM_25, OUTPUT);
+
+  // Start out our event loop...
   Serial.println("\tHello Braun Series 8 Dock Event Loop...");
-  if(time > next_pressed) {
+  
+  // If debug loop (press button every 10 seconds) is enabled, run this block.
+  if(debug_loop && time > next_pressed) {
+    
+    // Print the time again, and that we're going to press the button.
     Serial.print(time);
     Serial.println("\tPressing that button...");
+    
+    // Set the next button press time to 10 seconds in the future.
     next_pressed += 10000;
+
+    // Press the button for 0.8 seconds.
     digitalWrite(GPIO_NUM_25, HIGH);
     delay(800);
     digitalWrite(GPIO_NUM_25, LOW);
